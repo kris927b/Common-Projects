969d3bff411288554f8d35de852ce49b7f9905a2
Kristian Nørgaard Jensen <kristian.smed.noergaard@gmail.com>
2017-04-21 10:58:16 +0200

    Created project for the digital part

---
+++ AudioAmplifier/AudioAmplifier/AudioAmplifier/main.c
@@ -0,0 +1,25 @@
+/*
+ * AudioAmplifier.c
+ *
+ * Created: 4/21/2017 10:44:40 AM
+ * Author : Matej Majtan, Domenico Vilani & Kristian Nørgaard Jensen
+ */ 
+
+#include <avr/io.h>
+
+/* Our own libraries, found in the Libs folder in the project */
+#include "Libs/Dig_Pot_Lib.h" //Library for the digital pot meter
+#include "Libs/SPI_Lib.h" //Library for SPI functionality
+#include "Libs/std_lib.h" //Library for common functions that are useful
+#include "Libs/usart_lib.h" //Library for Usart
+
+int main(void)
+{
+	
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
