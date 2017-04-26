f17658efaad075ce7d87092bbe5738591ea98d99
Kristian Nørgaard Jensen <kristian.smed.noergaard@gmail.com>
2017-04-26 15:35:23 +0200

    SPI

--- AudioAmplifier/AudioAmplifier/Testing_Project/main.c
+++ AudioAmplifier/AudioAmplifier/Testing_Project/main.c
@@ -8,18 +8,29 @@
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
-#include <avr/delay.h>
 #include "Dig_Pot_Lib.h"
 #include "std_lib.h"
 
 
 int main(void)
 {
-	Init_Pot_Meter();
-	Write_Pot_Meter(10, CS_1);
-    /* Replace with your application code */
-    while (1) 
-    {
+	/* Set the MOSI & SCK as Output lines */
+	DDRB |= (1 << PINB3) | (1 << PINB5) | (1 << PINB0);
+	//DDRB &= ~(1 << PINB4);
+	//DDRD |= (1 << PIND0);
+	/* Set the SCK pin to low */
+	//PORTB &= ~(1 << PINB5);
+	PORTB |= (1 << PINB0);
+	//PORTD &= ~(1 << PIND0);
+	/* Enable SPI */
+	SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+
+    while (1) {
+		PORTB &= ~(1 << PINB0);
+		SPDR = 0b00100100;
+		while(!(SPSR & (1 << SPIF)));
+		PORTB |= (1 << PINB0);
+		delay(300);
     }
 }
 
